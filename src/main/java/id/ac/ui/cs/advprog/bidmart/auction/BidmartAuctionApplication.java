0535d8c4b70d74393d19eb0b9aee2cd749e70c72
kurrorro <keishavnia@gmail.com>
2026-02-20 14:02:02 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/auction/BidmartAuctionApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmart.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartAuctionApplication.class, args);
+    }
+
+}
